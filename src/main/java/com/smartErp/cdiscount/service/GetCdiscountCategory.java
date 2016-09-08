6f14194b317aac1afcb462b6c3ad23180f0f4af1
oracle123 <312651213@qq.com>
2016-09-08 22:30:44 +0800

    2028

--- src/main/java/com/smartErp/cdiscount/service/GetCdiscountCategory.java
+++ src/main/java/com/smartErp/cdiscount/service/GetCdiscountCategory.java
@@ -5,31 +5,14 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
-import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 
-import com.cdiscount.ws.stub.MarketplaceAPIServiceStub;
 import com.cdiscount.ws.stub.MarketplaceAPIServiceStub.CategoryTree;
-import com.cdiscount.ws.stub.MarketplaceAPIServiceStub.CategoryTreeMessage;
-import com.cdiscount.ws.stub.MarketplaceAPIServiceStub.GetAllowedCategoryTreeResponse;
-import com.cdiscount.ws.stub.MarketplaceAPIServiceStub.HeaderMessage;
 import com.google.gson.Gson;
-import com.tomtop.application.dao.mongo.sale.cdiscount.CdiscountCategoryDao;
-import com.tomtop.application.dao.mongo.sale.cdiscount.CdiscountDao;
-import com.tomtop.application.libraries.MyDate;
-import com.tomtop.application.orm.mongo.sale.cdiscount.CdiscountApiConfig;
-import com.tomtop.application.orm.mongo.sale.cdiscount.CdiscountCategory;
-import com.tomtop.script.cdiscount.CdiscountHeaderMessageUtil;
-import com.tomtop.script.cdiscount.CdiscountTokenUtil;
+import com.smartErp.util.code.Dumper;
 
 public class GetCdiscountCategory {
-	
-	private MyDate myDate = new MyDate();
-	private static Integer count = 0;
 	
 	public static void main(String[] args) {
 		GetCdiscountCategory getCdiscountCategory = new GetCdiscountCategory();
@@ -40,61 +23,59 @@ public class GetCdiscountCategory {
 		GetCdiscountCategory getCdiscountCategory = new GetCdiscountCategory();
 		CategoryTree initCategoryTree = getCdiscountCategory.initCdiscountCategoryTree();
 		CategoryTree allCategoryTree[] = initCategoryTree.getChildrenCategoryList().getCategoryTree();
-		Integer categoryLevel = 1;
-		CdiscountCategoryDao cdiscountCategoryDao = new CdiscountCategoryDao();
-		Map<String, Object> deleteMap = new HashMap<String, Object>();
-		cdiscountCategoryDao.delete(deleteMap);
-		for (CategoryTree categoryTree : allCategoryTree) {
-			createCdiscountCategory(0, categoryLevel ,categoryTree);
-		}
+		Dumper.dump(allCategoryTree);
+//		Integer categoryLevel = 1;
+//		for (CategoryTree categoryTree : allCategoryTree) {
+//			createCdiscountCategory(0, categoryLevel ,categoryTree);
+//		}
 	}
 	
-	public boolean runScript(String argList) { 
-		String email = argList;
-		if (StringUtils.isEmpty(email)) {
-			System.out.println("email is empty !");
-			return false;
-		}
-		CdiscountDao cdiscountDao = new CdiscountDao();
-		Map<String, Object> queryMap = new HashMap<String, Object>();
-		queryMap.put("toEmail", email);
-		CdiscountApiConfig cdiscountApiConfig = null;
-		List<CdiscountApiConfig> cdiscountApiConfigList = cdiscountDao.fetchList(queryMap);
-		if (CollectionUtils.isEmpty(cdiscountApiConfigList)) {
-			System.out.println("cdiscountApiConfigList is empty !");
-			return false;
-		} else {
-			cdiscountApiConfig = cdiscountApiConfigList.get(0);
-		}
-		
-		if (null != cdiscountApiConfig) {
-			try {
-				MarketplaceAPIServiceStub marketplaceAPIServiceStub = new MarketplaceAPIServiceStub();
-				MarketplaceAPIServiceStub.GetAllowedCategoryTree parmaGetAllowedCategoryTree = new MarketplaceAPIServiceStub.GetAllowedCategoryTree();
-				String token = CdiscountTokenUtil.getCashToken(cdiscountApiConfig);
-				HeaderMessage headerMessage = CdiscountHeaderMessageUtil.getHeaderMessage(cdiscountApiConfig, token);
-				parmaGetAllowedCategoryTree.setHeaderMessage(headerMessage);
-				GetAllowedCategoryTreeResponse response = marketplaceAPIServiceStub.getAllowedCategoryTree(parmaGetAllowedCategoryTree);
-				CategoryTreeMessage categoryTreeMessage = response.getGetAllowedCategoryTreeResult();
-				CategoryTree initCategoryTree = categoryTreeMessage.getCategoryTree();
-				CategoryTree allCategoryTree[] = initCategoryTree.getChildrenCategoryList().getCategoryTree();
-				Integer categoryLevel = 1;
-				CdiscountCategoryDao cdiscountCategoryDao = new CdiscountCategoryDao();
-				Map<String, Object> deleteMap = new HashMap<String, Object>();
-				cdiscountCategoryDao.delete(deleteMap);
-				for (CategoryTree categoryTree : allCategoryTree) {
-					createCdiscountCategory(0, categoryLevel ,categoryTree);
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		return true;
-	}
+//	public boolean runScript(String argList) { 
+//		String email = argList;
+//		if (StringUtils.isEmpty(email)) {
+//			System.out.println("email is empty !");
+//			return false;
+//		}
+//		CdiscountDao cdiscountDao = new CdiscountDao();
+//		Map<String, Object> queryMap = new HashMap<String, Object>();
+//		queryMap.put("toEmail", email);
+//		CdiscountApiConfig cdiscountApiConfig = null;
+//		List<CdiscountApiConfig> cdiscountApiConfigList = cdiscountDao.fetchList(queryMap);
+//		if (CollectionUtils.isEmpty(cdiscountApiConfigList)) {
+//			System.out.println("cdiscountApiConfigList is empty !");
+//			return false;
+//		} else {
+//			cdiscountApiConfig = cdiscountApiConfigList.get(0);
+//		}
+//		
+//		if (null != cdiscountApiConfig) {
+//			try {
+//				MarketplaceAPIServiceStub marketplaceAPIServiceStub = new MarketplaceAPIServiceStub();
+//				MarketplaceAPIServiceStub.GetAllowedCategoryTree parmaGetAllowedCategoryTree = new MarketplaceAPIServiceStub.GetAllowedCategoryTree();
+//				String token = CdiscountTokenUtil.getCashToken(cdiscountApiConfig);
+//				HeaderMessage headerMessage = CdiscountHeaderMessageUtil.getHeaderMessage(cdiscountApiConfig, token);
+//				parmaGetAllowedCategoryTree.setHeaderMessage(headerMessage);
+//				GetAllowedCategoryTreeResponse response = marketplaceAPIServiceStub.getAllowedCategoryTree(parmaGetAllowedCategoryTree);
+//				CategoryTreeMessage categoryTreeMessage = response.getGetAllowedCategoryTreeResult();
+//				CategoryTree initCategoryTree = categoryTreeMessage.getCategoryTree();
+//				CategoryTree allCategoryTree[] = initCategoryTree.getChildrenCategoryList().getCategoryTree();
+//				Integer categoryLevel = 1;
+//				CdiscountCategoryDao cdiscountCategoryDao = new CdiscountCategoryDao();
+//				Map<String, Object> deleteMap = new HashMap<String, Object>();
+//				cdiscountCategoryDao.delete(deleteMap);
+//				for (CategoryTree categoryTree : allCategoryTree) {
+//					createCdiscountCategory(0, categoryLevel ,categoryTree);
+//				}
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//			}
+//		}
+//		return true;
+//	}
 	
 	public CategoryTree initCdiscountCategoryTree(){
 		CategoryTree categoryTree = null;
-		String path = "/home/tomtop2028/cdiscount.txt";
+		String path = "D:\\cdiscount.txt";
 		File file = new File(path);
 		BufferedReader reader = null;
 		try {
@@ -120,36 +101,36 @@ public class GetCdiscountCategory {
 		return categoryTree;
 	} 
 	
-	private void createCdiscountCategory(Integer parentId, Integer categoryLevel,
-			CategoryTree categoryTree) {
-		CdiscountCategory cdiscountCategory = new CdiscountCategory();
-		cdiscountCategory.setCategoryCode(categoryTree.getCode());
-		cdiscountCategory.setName(categoryTree.getName());
-		cdiscountCategory.setCategoryLevel(categoryLevel);
-		cdiscountCategory.setAllowOfferIntegration(categoryTree.getAllowOfferIntegration());
-		cdiscountCategory.setAllowProductIntegration(categoryTree.getAllowProductIntegration());
-		cdiscountCategory.setParentId(parentId);
-		cdiscountCategory.setUpdateTime(myDate.getCurrentDateTime());
-		CategoryTree childCategoryTree[] = categoryTree.getChildrenCategoryList().getCategoryTree();
-		if (null != childCategoryTree && childCategoryTree.length > 0) {
-			cdiscountCategory.setIsParent(1);
-		} else {
-			cdiscountCategory.setIsParent(0);
-		}
-		Integer saveCategoryId = saveCdiscountCategory(cdiscountCategory);
-		if (null != childCategoryTree && childCategoryTree.length > 0) {
-			for (CategoryTree childCategoryTreetmp : childCategoryTree) {
-				createCdiscountCategory(saveCategoryId, categoryLevel + 1, childCategoryTreetmp);
-			}
-		}
-	}
-
-	private Integer saveCdiscountCategory(CdiscountCategory cdiscountCategory) {
-		count++;
-		CdiscountCategoryDao cdiscountCategoryDao = new CdiscountCategoryDao();
-		cdiscountCategory.setId(count);
-		cdiscountCategoryDao.saveCategory(cdiscountCategory);
-		return count;
-	}
+//	private void createCdiscountCategory(Integer parentId, Integer categoryLevel,
+//			CategoryTree categoryTree) {
+//		CdiscountCategory cdiscountCategory = new CdiscountCategory();
+//		cdiscountCategory.setCategoryCode(categoryTree.getCode());
+//		cdiscountCategory.setName(categoryTree.getName());
+//		cdiscountCategory.setCategoryLevel(categoryLevel);
+//		cdiscountCategory.setAllowOfferIntegration(categoryTree.getAllowOfferIntegration());
+//		cdiscountCategory.setAllowProductIntegration(categoryTree.getAllowProductIntegration());
+//		cdiscountCategory.setParentId(parentId);
+//		cdiscountCategory.setUpdateTime(myDate.getCurrentDateTime());
+//		CategoryTree childCategoryTree[] = categoryTree.getChildrenCategoryList().getCategoryTree();
+//		if (null != childCategoryTree && childCategoryTree.length > 0) {
+//			cdiscountCategory.setIsParent(1);
+//		} else {
+//			cdiscountCategory.setIsParent(0);
+//		}
+//		Integer saveCategoryId = saveCdiscountCategory(cdiscountCategory);
+//		if (null != childCategoryTree && childCategoryTree.length > 0) {
+//			for (CategoryTree childCategoryTreetmp : childCategoryTree) {
+//				createCdiscountCategory(saveCategoryId, categoryLevel + 1, childCategoryTreetmp);
+//			}
+//		}
+//	}
+//
+//	private Integer saveCdiscountCategory(CdiscountCategory cdiscountCategory) {
+//		count++;
+//		CdiscountCategoryDao cdiscountCategoryDao = new CdiscountCategoryDao();
+//		cdiscountCategory.setId(count);
+//		cdiscountCategoryDao.saveCategory(cdiscountCategory);
+//		return count;
+//	}
 	
 }
--- src/main/java/com/smartErp/frame/controller/MainPageController.java
+++ src/main/java/com/smartErp/frame/controller/MainPageController.java
@@ -2,14 +2,19 @@ package com.smartErp.frame.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.smartErp.system.model.User;
+import com.smartErp.system.service.UserService;
 
 @Controller
 @RequestMapping("SmartErp")
 public class MainPageController {
+	
+	@Autowired
+	UserService userService;
 	
 	@RequestMapping("home")
 	public String goToMainPage() {
@@ -25,8 +30,11 @@ public class MainPageController {
 	
 	@RequestMapping("login")
 	public String login(HttpServletRequest request, String username, String password) {
-		User user = new User();
-		request.getSession().setAttribute("user", user);
+		username = "2028";
+		User user = userService.getUserByUserName(username);
+		if (null != user) {
+			request.getSession().setAttribute("user", user);
+		}
 		return "redirect:/SmartErp/home"; 
 	}
 	
